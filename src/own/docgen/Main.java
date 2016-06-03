570a158430a2745fb126d95993113572f937add9
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-06-03 23:43:01 +0300

    use Reflection API to get the information about MathUtils class's methods

--- src/own/docgen/Main.java
+++ src/own/docgen/Main.java
@@ -1,7 +1,35 @@
 package own.docgen;
 
+import own.math.MathUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class Main {
+    // TODO: Process the MathUtils class's annotations
+
     public static void main(String[] args) {
-        // TODO: Process the MathUtils class's annotations
+        // Get a class object
+        Class<?> clazz = MathUtils.class;
+
+        // Get all declared methods
+        Method[] methods = clazz.getDeclaredMethods();
+
+        // Loop over methods
+        for (Method method : methods) {
+            // Display method name
+            System.out.printf("name: %s%n", method.getName());
+
+            //Display parameter count
+            System.out.printf("\t# params: %s%n", method.getParameterCount());
+
+            //Display return type
+            System.out.printf("\treturn type: %s%n", method.getReturnType().getSimpleName());
+
+            // Display modifiers
+            int mods = method.getModifiers();
+            String modsStr = Modifier.toString(mods);
+            System.out.printf("\tmodifiers: %s%n", modsStr);
+        }
     }
 }
